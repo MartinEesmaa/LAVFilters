508f2f6d39fbabdf3a4f21b5fca8d5ab1c24437d
clsid2 <4704996+clsid2@users.noreply.github.com>
2023-12-06 14:06:14 +0100

    Add several mediatypes

--- include/LAVVideoSettings.h
+++ include/LAVVideoSettings.h
@@ -93,6 +93,8 @@ typedef enum LAVVideoCodec
     Codec_CineformHD,
     Codec_MagicYUV,
     Codec_AV1,
+    Codec_VP4,
+    Codec_FIC,
 
     Codec_VideoNB // Number of entries (do not use when dynamically linking)
 } LAVVideoCodec;
